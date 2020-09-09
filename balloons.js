5367b57d1c868e935f571438b0663bc17746342d
cpavelsek <57453934+cpavelsek@users.noreply.github.com>
2020-09-09 17:26:06 -0500

    Add files via upload

---
+++ balloons.js
@@ -0,0 +1,100 @@
+﻿$(function () {
+    $('#birthday').pickadate({ format: 'mmmm, d' });
+});
+
+// uncheck all checkboxes (FireFox)
+$('.form-check-input').each(function () {
+    $(this).prop('checked', false);
+});
+
+// event listener for check/uncheck
+$('.form-check-input').on('change', function () {
+    // make the image visible
+    $('#' + this.id + 'Img').css('visibility', 'visible')
+    // animate balloon in/out based on checkbox
+    $(this).is(':checked') ?
+        $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
+        $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+});
+
+
+//select ALL Balloons
+$(function () {
+    $('#submitCheck').on('click', function (evt) {
+        $('.form-check-input').each(function () {
+            $(this).prop('checked', true);
+
+                // make the image visible
+                $('#' + this.id + 'Img').css('visibility', 'visible')
+                // animate balloon in/out based on checkbox
+                $(this).is(':checked') ?
+                    $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
+                    $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+ 
+        });
+
+    })
+})
+
+
+
+
+//generate random birthday animations
+$(function () {
+    
+    var animateArray = ['animated bounce', 'animated flash', 'animated pulse', 'animated rubberBand', 'animated shakeX',
+        'animated shakeY', 'animated headShake', 'animated swing', 'animated tada', 'animated wobble', 'animated jello', 'animated heartBeat'];
+    var random = animateArray[Math.floor((Math.random() * animateArray.length))];
+
+    console.log(random);
+    document.getElementById("birthdayFlash").className = random;
+    
+})
+
+//display popup
+$(function () {
+
+        $('#submit').on('click', function (e) {
+
+    
+            $('#toast').toast({ autohide: false }).toast('show');
+
+
+            $('#product').html($(this).data('product'));
+
+        })
+})
+
+//hover colors for h1 - red
+document.getElementById("red").addEventListener("mouseover", function () {
+    document.getElementById("birthdayFlash").style.color = "red";
+})
+document.getElementById("red").addEventListener("mouseout", function () {
+    document.getElementById("birthdayFlash").style.color = "slategray";
+})
+//hover colors for h1 - blue
+document.getElementById("blue").addEventListener("mouseover", function () {
+    document.getElementById("birthdayFlash").style.color = "blue";
+})
+document.getElementById("blue").addEventListener("mouseout", function () {
+    document.getElementById("birthdayFlash").style.color = "slategray";
+})
+//hover colors for h1 - green
+document.getElementById("green").addEventListener("mouseover", function () {
+    document.getElementById("birthdayFlash").style.color = "green";
+})
+document.getElementById("green").addEventListener("mouseout", function () {
+    document.getElementById("birthdayFlash").style.color = "slategray";
+})
+
+//$(function () {
+//   $('#submit').on('click', function (e) {
+
+ //       $('#submit').toast('show');
+ //       $('#toast').toast({ autohide: false }).toast('show');
+
+
+//        $('#product').html($(this).data('product'));
+//        $('#code').html($(this).data('code'));
+//    })
+//})
---
+++ client.js
@@ -0,0 +1,35 @@
+﻿$(function () {
+    // preload audio
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function (e) {
+        e.preventDefault();
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+
+
+        $('#product').html($(this).data('product'));
+        $('#code').html($(this).data('code'));
+    });
+
+    var toastPopUp = document.querySelector('#toast')
+
+    window.addEventListener('keydown', function (evt) {
+        if (evt.key === "Escape") {
+            toastPopUp.style.display='none'
+        }
+    })
+
+    $("#seasoning").click(function () {
+        $("#product").data("Chef", "Chef");
+    })
+
+
+    $('#toast').toast({ autohide: false }).toast('show');
+
+});
